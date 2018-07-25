421e36feb2a679f74506d20dda6917c92e9998cc
Alex Hill <alex.hill@gmail.com>
2018-07-25 10:18:19 +0100

    update readme

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 # Builds site, commits and pushes to Github
-# This will push to whatever branch you have checked out in the submodule
+# This will checkout and commit to the master branch of the submodule
 
 if [[ ($# -eq 0) ]]; then
     echo 'Usage: ./deploy.sh "commit message here"'
@@ -11,6 +11,8 @@ fi
 here=$(dirname $0)
 
 ( cd $here/src && bundle exec jekyll build )
+
+( cd $here/site && git checkout master )
 
 cp $here/src/_site/* -r $here/site
 
